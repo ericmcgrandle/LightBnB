d63e44aa1203491ede201bf9057642d3a7b1078f
Eric McGrandle <ericmcgrandle@gmail.com>
2020-10-15 00:38:27 +0000

    get reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -68,7 +68,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+    SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating
+    FROM properties
+    JOIN reservations ON (properties.id = reservations.property_id)
+    JOIN property_reviews ON (properties.id = property_reviews.property_id)
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    HAVING reservations.end_date < NOW()
+    ORDER BY reservations.start_date
+    LIMIT $2;
+    `, [guest_id, limit])
+    .then(res => res.rows)
+    .catch(err => console.log('err', err));
 }
 exports.getAllReservations = getAllReservations;
 
